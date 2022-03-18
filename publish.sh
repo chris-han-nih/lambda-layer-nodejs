cfcd53cc233c6553f5d9c035bf1d1d81c22cbf31
chris.han <namilhan@gmail.com>
2022-03-18 23:07:19 +0900

    publish shell script refactoring 하라

--- publish.sh
+++ publish.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-cd $1
+PROD_NAME=$1
+DESCRIPTION=$2
 
-/bin/mkdir -p nsus
+echo $'\360\237\232\252' + 'Move to package directory'
+cd $PROD_NAME
 
-/bin/cp *.js nsus/
+if [ -f "package.json" ]; then
+  /bin/rm -rf node_modules
+  echo $'\342\232\231' + 'Install node package'
+  npm install
+fi
 
-/bin/cp nsus/* node_modules/
-
-/bin/mkdir nodejs
-
-/bin/cp node_modules/* nodejs/
+echo $'\360\237\223\213' + 'Packaging lambda layer package'
+/bin/mkdir -p nsus && /bin/cp *.js nsus/
+/bin/mv nsus node_modules/
+/bin/mkdir nodejs && /bin/mv node_modules nodejs/
 
 /usr/bin/zip nodejs.zip -r nodejs
 
-aws lambda publish-layer-version --layer-name $1 --description "$2" --zip-file fileb://nodejs.zip --compatible-runtimes nodejs14.x
+echo $'\342\217\261' + 'Publish package to lambda layer'
+ aws lambda publish-layer-version --layer-name $PROD_NAME --description "$DESCRIPTION" --zip-file fileb://nodejs.zip --compatible-runtimes nodejs14.x
 
-/bin/rm -rf nsus
-
+echo $'\360\237\227\221' + 'Clean up directory and file'
 /bin/rm -rf nodejs
-
 /bin/rm nodejs.zip
